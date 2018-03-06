be95e28b7b9b5b0514c82b7f55c9642169115ef2
contrafado <30272863+contrafado@users.noreply.github.com>
2018-03-06 16:36:28 -0500

    Map now renders, walls block movement, tut 2 done

--- render_functions.py
+++ render_functions.py
@@ -1,9 +1,20 @@
 import libtcodpy as libtcod 
 
-def render_all(con, entities, screen_width, screen_height):
+def render_all(con, entities, game_map, screen_width, screen_height, colors):
+    #draw all tiles in game map
+    for y in range(game_map.height):
+        for x in range(game_map.width):
+            wall = game_map.tiles[x][y].block_sight
+
+            if wall:
+                libtcod.console_set_char_background(con, x, y, colors.get('dark_wall'), libtcod.BKGND_SET)
+            else:
+                libtcod.console_set_char_background(con, x, y, colors.get('dark_ground'), libtcod.BKGND_SET)
+
     #draw all entities in the list
-	for entity in entities:
-	    draw_entity(con, entity)
+    for entity in entities:
+        draw_entity(con, entity)
+    libtcod.console_blit(con, 0, 0, screen_width, screen_height, 0, 0, 0)		
 		
 def clear_all(con, entities):
     for entity in entities: 
