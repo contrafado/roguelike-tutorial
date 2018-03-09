957bbd2e2c9f7045aea8d29767ccc6c1a2b71cb9
contrafado <30272863+contrafado@users.noreply.github.com>
2018-03-09 12:45:42 -0500

    Enemy generation implemented

--- render_functions.py
+++ render_functions.py
@@ -1,8 +1,9 @@
-import libtcodpy as libtcod 
+import libtcodpy as libtcod
+
 
 def render_all(con, entities, game_map, fov_map, fov_recompute, screen_width, screen_height, colors):
-    #draw all tiles in game map
     if fov_recompute:
+    # Draw all the tiles in the game map
         for y in range(game_map.height):
             for x in range(game_map.width):
                 visible = libtcod.map_is_in_fov(fov_map, x, y)
@@ -13,6 +14,7 @@ def render_all(con, entities, game_map, fov_map, fov_recompute, screen_width, sc
                         libtcod.console_set_char_background(con, x, y, colors.get('light_wall'), libtcod.BKGND_SET)
                     else:
                         libtcod.console_set_char_background(con, x, y, colors.get('light_ground'), libtcod.BKGND_SET)
+
                     game_map.tiles[x][y].explored = True
                 elif game_map.tiles[x][y].explored:
                     if wall:
@@ -20,20 +22,23 @@ def render_all(con, entities, game_map, fov_map, fov_recompute, screen_width, sc
                     else:
                         libtcod.console_set_char_background(con, x, y, colors.get('dark_ground'), libtcod.BKGND_SET)
 
-    #draw all entities in the list
+    # Draw all entities in the list
     for entity in entities:
         draw_entity(con, entity, fov_map)
-    libtcod.console_blit(con, 0, 0, screen_width, screen_height, 0, 0, 0)		
-		
+
+    libtcod.console_blit(con, 0, 0, screen_width, screen_height, 0, 0, 0)
+
+
 def clear_all(con, entities):
-    for entity in entities: 
+    for entity in entities:
         clear_entity(con, entity)
 
 def draw_entity(con, entity, fov_map):
     if libtcod.map_is_in_fov(fov_map, entity.x, entity.y):
         libtcod.console_set_default_foreground(con, entity.color)
         libtcod.console_put_char(con, entity.x, entity.y, entity.char, libtcod.BKGND_NONE)
-	
+
+
 def clear_entity(con, entity):
-    #erase the character that represents the object
-	libtcod.console_put_char(con, entity.x, entity.y, ' ', libtcod.BKGND_NONE)
+    # erase the character that represents this object
+    libtcod.console_put_char(con, entity.x, entity.y, ' ', libtcod.BKGND_NONE)
