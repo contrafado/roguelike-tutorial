2dcf2764d21d4cbf42b7da99db9a89da473b674f
contrafado <30272863+contrafado@users.noreply.github.com>
2018-03-14 16:48:46 -0400

    attack spells incl with targeting

--- input_handlers.py
+++ input_handlers.py
@@ -10,6 +10,8 @@ def handle_keys(key, game_state):
         return handle_player_dead_keys(key)
     elif game_state in (GameStates.SHOW_INVENTORY, GameStates.DROP_INVENTORY):
         return handle_inventory_keys(key)
+    elif game_state == GameStates.TARGETING:
+        return handle_targeting_keys(key)
 
     return {}
 
@@ -54,7 +56,6 @@ def handle_player_turn_keys(key):
     # No key was pressed
     return {}
 
-
 def handle_player_dead_keys(key):
     key_char = chr(key.c)
 
@@ -70,7 +71,6 @@ def handle_player_dead_keys(key):
 
     return {}
 
-
 def handle_inventory_keys(key):
     index = key.c - ord('a')
 
@@ -83,5 +83,21 @@ def handle_inventory_keys(key):
     elif key.vk == libtcod.KEY_ESCAPE:
         # Exit the menu
         return {'exit': True}
+
+    return {}
+
+def handle_targeting_keys(key):
+    if key.vk == libtcod.KEY_ESCAPE:
+        return {'exit': True}
+
+    return {}
+
+def handle_mouse(mouse):
+    (x, y) = (mouse.cx, mouse.cy)
+
+    if mouse.lbutton_pressed:
+        return {'left_click': (x, y)}
+    elif mouse.rbutton_pressed:
+        return {'right_click': (x, y)}
 
     return {}
